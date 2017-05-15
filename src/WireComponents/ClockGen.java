cd6aa97ac5d6abc29257f0410ad931f9d0551084
losiu97 <konrad.lys1997@gmail.com>
2017-05-15 18:42:40 +0200

    Bug fix

--- src/WireComponents/ClockGen.java
+++ src/WireComponents/ClockGen.java
@@ -8,24 +8,29 @@ public class ClockGen extends Component {
         this.wire=connected;
         this.loc = loc;
         this.rotation = rotation;
+        this.input= new int[][] {};
         switch (rotation) {
             case 0:
                 this.structure = new int[][]{{0, 2, 1,3, 3,0},
                         {3, 0, 0, 0,0,3},
                         {0, 3, 3, 3,3,0}};
+                this.output = new int[][]{{1,5}};
                 break;
             case 1:
                 this.structure = new int[][]{{0,3,0},{3,0,2},{3,0,1},{3,0,3},{3,0,3},{0,3,0}
                         };
+                this.output = new int[][]{{5,1}};
                 break;
             case 2:
                 this.structure = new int[][]{{0, 2, 1,3, 3,0},
                         {3, 0, 0, 0,0,3},
                         {0, 3, 3, 3,3,0}};
+                this.output = new int[][]{{1,0}};
                 break;
             case 3:
                 this.structure = new int[][]{{0,3,0},{3,0,2},{3,0,1},{3,0,3},{3,0,3},{0,3,0}
                 };
+                this.output = new int[][]{{0,1}};
                 break;
         }
 
